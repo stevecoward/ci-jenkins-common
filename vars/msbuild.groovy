7537e438753d8489edde8521b0b99b5076d23d87
stevecward <steve.coward@gmail.com>
2020-12-13 12:57:29 -0500

    adds workspace env to msbuild solution

--- vars/msbuild.groovy
+++ vars/msbuild.groovy
@@ -1,6 +1,7 @@
 void call(String solution, String targetFrameworkVersion) {
+    env.WORKSPACE = pwd()
     String msbuild = 'C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\MSBuild.exe'
-    Boolean exitStatus = bat(returnStatus: true, script: "${msbuild} ${solution} -toolsVersion:${targetFrameworkVersion}")
+    Boolean exitStatus = bat(returnStatus: true, script: "${msbuild} ${env.WORKSPACE}\\${solution} -toolsVersion:${targetFrameworkVersion}")
 
     if (exitStatus != 0) {
         currentBuild.result = 'FAILURE'
